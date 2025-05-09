cd3e96aad0e2b6f53cf2905b5d421c1698b200c3
0xrajath <rajathalex@gmail.com>
2025-05-09 19:00:30 -0400

    chore: output json

--- script/local/DeployTaskMailbox.s.sol
+++ script/local/DeployTaskMailbox.s.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.27;
 
 import {Script, console} from "forge-std/Script.sol";
+import {stdJson} from "forge-std/StdJson.sol";
 
 import {TaskMailbox} from "@hourglass-monorepo/src/core/TaskMailbox.sol";
 
 contract DeployTaskMailbox is Script {
+    using stdJson for string;
+
     function setUp() public {}
 
     function run() public {
@@ -21,5 +24,27 @@ contract DeployTaskMailbox is Script {
         console.log("TaskMailbox deployed to:", address(taskMailbox));
 
         vm.stopBroadcast();
+
+        // Add the addresses object
+        string memory addresses = "addresses";
+        addresses = vm.serializeAddress(addresses, "taskMailbox", address(taskMailbox));
+
+        // Add the chainInfo object
+        string memory chainInfo = "chainInfo";
+        vm.serializeUint(chainInfo, "chainId", block.chainid);
+        chainInfo = vm.serializeUint(chainInfo, "deploymentBlock", block.number);
+
+        // Add parameters object
+        string memory emptyParams = "{}";
+
+        // Finalize the JSON
+        string memory finalJson = "final";
+        vm.serializeString(finalJson, "addresses", addresses);
+        vm.serializeString(finalJson, "chainInfo", chainInfo);
+        finalJson = vm.serializeString(finalJson, "parameters", emptyParams);
+
+        // Write to output file
+        string memory outputFile = string.concat("script/local/", "output/deploy_hourglass_core_output.json");
+        vm.writeJson(finalJson, outputFile);
     }
 }
