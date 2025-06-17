ce37b420e587640bc7baa7567a2eb73938d83b3c
Grezle <4310551+grezle@users.noreply.github.com>
2025-06-17 12:16:16 +0100

    fix: pass keyRegistrar to TaskAVSRegistrar constructor

--- script/devnet/deploy/DeployAVSL1Contracts.s.sol
+++ script/devnet/deploy/DeployAVSL1Contracts.s.sol
@@ -5,10 +5,12 @@ import {Script, console} from "forge-std/Script.sol";
 
 import {IAllocationManager} from "@eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 
+import {IKeyRegistrar} from "@eigenlayer-contracts/src/contracts/interfaces/IKeyRegistrar.sol";
+
 import {TaskAVSRegistrar} from "@project/l1-contracts/TaskAVSRegistrar.sol";
 
 contract DeployAVSL1Contracts is Script {
-    function run(string memory environment, address avs, address allocationManager) public {
+    function run(string memory environment, address avs, address allocationManager, address keyRegistrar) public {
         // Load the private key from the environment variable
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_DEPLOYER");
         address deployer = vm.addr(deployerPrivateKey);
@@ -17,7 +19,7 @@ contract DeployAVSL1Contracts is Script {
         vm.startBroadcast(deployerPrivateKey);
         console.log("Deployer address:", deployer);
 
-        TaskAVSRegistrar taskAVSRegistrar = new TaskAVSRegistrar(avs, IAllocationManager(allocationManager));
+        TaskAVSRegistrar taskAVSRegistrar = new TaskAVSRegistrar(avs, IAllocationManager(allocationManager), IKeyRegistrar(keyRegistrar));
         console.log("TaskAVSRegistrar deployed to:", address(taskAVSRegistrar));
 
         vm.stopBroadcast();
